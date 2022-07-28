bca4adaa16f8de93e1c012d5faf290211a63e8ec
rodrigoleonello <rodrigosleonello@gmail.com>
2022-07-28 18:00:06 -0300

    feat: initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace dummy_project;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
